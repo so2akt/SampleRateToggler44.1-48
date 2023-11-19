a11de2b2159ea17ebff5a1b5091cc747b05d1125
so2akt <17232984+so2akt@users.noreply.github.com>
2023-11-20 02:48:07 +0900

    add audio input device enumeration show

--- SampleRateToggler44.1-48.cpp
+++ SampleRateToggler44.1-48.cpp
@@ -1,20 +1,103 @@
-// SampleRateToggler44.1-48.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include <wchar.h>
+#include "mmdeviceapi.h"
+#include "Propidl.h"
+#include "functiondiscoverykeys_devpkey.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HRESULT hRes = NULL;
+	IMMDeviceEnumerator *pDevEnum = NULL;
+	IMMDeviceCollection *pDev = NULL;
+	UINT nCount = 0;
+	IMMDevice *pDevId = NULL;
+	LPWSTR wstrEndpointId = NULL;
+	IPropertyStore *pPropStore = NULL;
+	PROPVARIANT friendlyName;
+
+	hRes = CoInitialize(NULL);
+	if (FAILED(hRes))	/* TODO: refactor error proc*/
+	{
+		return EXIT_FAILURE;
+	}
+
+	hRes = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL,
+		CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (LPVOID*)&pDevEnum);
+	if (FAILED(hRes))	/* TODO: refactor error proc*/
+	{
+		CoUninitialize();
+		return EXIT_FAILURE;
+	}
+
+	/* get list of active input audio devices */
+	hRes = pDevEnum->EnumAudioEndpoints(eCapture/* eRender: output, eCapture: input */,
+		DEVICE_STATE_ACTIVE, &pDev);
+	if (FAILED(hRes))	/* TODO: refactor error proc*/
+	{
+		pDevEnum->Release();
+		CoUninitialize();
+		return EXIT_FAILURE;
+	}
+
+	hRes = pDev->GetCount(&nCount);
+	if (FAILED(hRes))	/* TODO: refactor error proc*/
+	{
+		pDev->Release();
+		pDevEnum->Release();
+		CoUninitialize();
+		return EXIT_FAILURE;
+	}
+	for (UINT i = 0; i < nCount; i++)
+	{
+		hRes = pDev->Item(i, &pDevId);
+		if (FAILED(hRes))	/* TODO: refactor error proc*/
+		{
+			pDev->Release();
+			pDevEnum->Release();
+			CoUninitialize();
+			return EXIT_FAILURE;
+		}
+
+		hRes = pDevId->GetId(&wstrEndpointId);
+		if (FAILED(hRes))	/* TODO: refactor error proc*/
+		{
+			pDevId->Release();
+			pDev->Release();
+			pDevEnum->Release();
+			CoUninitialize();
+			return EXIT_FAILURE;
+		}
+
+		hRes = pDevId->OpenPropertyStore(STGM_READ, &pPropStore);
+		if (FAILED(hRes))	/* TODO: refactor error proc*/
+		{
+			pDevId->Release();
+			pDev->Release();
+			pDevEnum->Release();
+			CoUninitialize();
+			return EXIT_FAILURE;
+		}
+
+		/* get friendly names on Sound settings */
+		PropVariantInit(&friendlyName);
+		hRes = pPropStore->GetValue(PKEY_Device_FriendlyName, &friendlyName);
+		if (FAILED(hRes))	/* TODO: refactor error proc*/
+		{
+			pPropStore->Release();
+			pDevId->Release();
+			pDev->Release();
+			pDevEnum->Release();
+			CoUninitialize();
+			return EXIT_FAILURE;
+		}
+		wprintf(L"Device No. %d: %ls\n", i, friendlyName.pwszVal);	/* FIXME: show multi-byte letters properly */
+		PropVariantClear(&friendlyName);
+	}
+
+	pPropStore->Release();
+	pDevId->Release();
+	pDev->Release();
+	pDevEnum->Release();
+	CoUninitialize();
+
+	return EXIT_SUCCESS;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
